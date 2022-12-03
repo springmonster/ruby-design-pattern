ee2559397d17e82103c226f101a9dac2ba4dc502
kuanghc1 <kuanghc1@lenovo.com>
2022-12-03 21:17:18 +0800

    add observer

---
+++ chapter_5_observer/observer.rb
@@ -0,0 +1,48 @@
+module Subject
+  def initialize
+    @observers = []
+  end
+
+  # 这里加入的是Block
+  def add_observer(&observer)
+    @observers << observer
+  end
+
+  def delete_observer(observer)
+    @observers.delete(observer)
+  end
+
+  def notify_observers
+    @observers.each do |observer|
+      observer.call(self)
+    end
+  end
+end
+
+class Employee
+  include Subject
+
+  attr_reader :name, :address
+  attr_reader :salary
+
+  def initialize(name, address, salary)
+    super()
+    @name = name
+    @address = address
+    @salary = salary
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+    notify_observers
+  end
+end
+
+fred = Employee.new('Fred', '123 Sesame Street', 100_000)
+
+fred.add_observer do |changed_employee|
+  puts("check for #{changed_employee.name}")
+  puts("salary is #{changed_employee.salary}")
+end
+
+fred.salary = 3000
