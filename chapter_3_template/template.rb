90fd6f061efa25d824adb0b3c640fd82c0982add
kuanghc1 <kuanghc1@lenovo.com>
2022-12-03 17:00:35 +0800

    add design patterns

---
+++ chapter_3_template/template.rb
@@ -0,0 +1,25 @@
+class Report
+  def initialize
+    @title = 'Monthly Report'
+    @text = ['Things are going', 'really, really well.']
+  end
+
+  def output_report
+    output_start
+    output_head
+    output_body_start
+    output_body
+    output_body_end
+    output_end
+  end
+
+  def output_body
+    @text.each do |line|
+      output_line(line)
+    end
+  end
+
+  def output_start
+    raise 'Called abstract method: output_start'
+  end
+end
